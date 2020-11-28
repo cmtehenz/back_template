d9583ab16018cbe114a90b2e6bfe0325d0b7b7c3
Gustavo Costa <cmtehenz@gmail.com>
2020-11-28 15:21:25 -0300

    :construction: Adicionado modulo user

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const routes = Router();
-
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello word' });
-});
-
-export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,46 @@
-import express from 'express';
-import routes from './routes';
+import 'reflect-metadata';
+import 'dotenv/config';
+
+import { errors } from 'celebrate';
+import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
+
+// import uploadConfig from '@config/upload';
+
+import AppError from '@shared/errors/AppError';
+
+// import rateLimiter from './middlewares/rateLimiter';
+// import '@shared/infra/typeorm';
+// import '@shared/container';
+
+import 'express-async-errors';
+import routes from './shared/infra/http/routes';
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+// app.use('/files', express.static(uploadConfig.uploadsFolder));
+// app.use(rateLimiter);
 app.use(routes);
+
+app.use(errors());
+
+// app.use(
+//   (err: Error, request: Request, response: Response, _next: NextFunction) => {
+//     if (err instanceof AppError) {
+//       return response.status(err.statusCode).json({
+//         status: 'error',
+//         message: err.message,
+//       });
+//     }
+//     console.log(err);
+//     return response.status(500).json({
+//       status: 'error',
+//       message: 'Internal server error',
+//     });
+//   },
+// );
 
 app.listen(3333, () => {
   console.log('⛵️ Server started on port 3333');
