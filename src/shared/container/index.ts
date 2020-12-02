5855d70573e84adff01253ebb7284ec00d7acab9
Gustavo Costa <cmtehenz@gmail.com>
2020-12-02 10:24:02 -0300

    :lock: Criado a recuperaçao de senha

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,9 +4,16 @@ import '@modules/users/providers';
 import './providers';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UsersTokenRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUsersTokenRepository from '@modules/users/repositories/IUserTokensRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUsersTokenRepository>(
+  'UserTokensRepository',
+  UsersTokensRepository,
 );
