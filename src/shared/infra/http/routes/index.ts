712a287e557bb4e7a9e6320387b3cb856e5020e6
Gustavo Costa <cmtehenz@gmail.com>
2020-12-02 09:33:52 -0300

    :rocket: Adicionado Show Profile e Update Profile

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
@@ -7,5 +8,6 @@ const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
