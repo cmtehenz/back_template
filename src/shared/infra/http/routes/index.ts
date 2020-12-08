b9f3ae822fff6d41f3544743318c7270d6b0772b
Gustavo Costa <cmtehenz@gmail.com>
2020-12-08 15:02:39 -0300

    :memo: criado readme e swagger

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import { swaggerDocument } from '@shared/docs/docSwagger';
 
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
@@ -7,6 +10,7 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
+routes.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
