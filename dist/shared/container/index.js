02afe736b9b8171bb1e3d9fa283d8dd3980b68ea
Gustavo Costa <cmtehenz@gmail.com>
2022-03-23 08:34:14 -0300

    add babel configs

---
+++ dist/shared/container/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _tsyringe = require("tsyringe");
+
+require("../../modules/users/providers");
+
+require("./providers");
+
+var _UsersRepository = _interopRequireDefault(require("../../modules/users/infra/typeorm/repositories/UsersRepository"));
+
+var _UsersTokenRepository = _interopRequireDefault(require("../../modules/users/infra/typeorm/repositories/UsersTokenRepository"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_tsyringe.container.registerSingleton('UsersRepository', _UsersRepository.default);
+
+_tsyringe.container.registerSingleton('UserTokensRepository', _UsersTokenRepository.default);
